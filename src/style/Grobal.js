c078cb1d6ea29aa5954549aaa920cdf973cb4283
sim7457 <simwjdgns3@naver.com>
2023-03-09 16:33:05 +0900

    230309

---
+++ src/style/Grobal.js
@@ -0,0 +1,44 @@
+import styled, { createGlobalStyle } from "styled-components";
+
+const Reset = createGlobalStyle`
+@import url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700;900&family=Roboto:wght@100;300;400;500;700;900&display=swap');
+* {
+    margin: 0;
+    padding: 0;
+    font-size: inherit;
+    font-weight: inherit;
+    color: inherit;
+    box-sizing: border-box;
+}
+*::before,
+*::after {
+    box-sizing: border-box;
+}
+ul,
+ol {
+    list-style: none;
+}
+em,
+i,
+address {
+    font-style: normal;
+}
+b,
+strong {
+    font-weight: normal;
+}
+a {
+    text-decoration: none;
+}
+img {
+    vertical-align: middle;
+    max-width: 100%;
+}
+body {
+    font-family: 'Roboto', 'Noto Sans KR', sans-serif;
+    line-height: 1;
+    color: #444;
+}
+`;
+
+export default Reset;
